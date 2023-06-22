6dc36b98ae079f6099999bb6a3e0c07243baa6c1
AVKomarov <extend@list.ru>
2023-06-22 21:01:36 +0300

    1.31

--- old/js/mymy.js
+++ old/js/mymy.js
@@ -12,19 +12,28 @@ c.addEventListener("click",pricelist3);
 
 function pricelist1(){
     elem = document.querySelector('.wrapper.avto');
+    blok = document.querySelector('.buttons-zakaz');
 elem.classList.toggle('vid');
+blok.classList.add('vid');
 document.querySelector('.wrapper.tech').classList.remove('vid');
 document.querySelector('.wrapper.travel').classList.remove('vid');
+// document.querySelector('.buttons-zakaz').classList.remove('vid');
 }
 function pricelist2(){
     elem = document.querySelector('.wrapper.tech');
+    blok = document.querySelector('.buttons-zakaz');
 elem.classList.toggle('vid');
+blok.classList.add('vid');
 document.querySelector('.wrapper.avto').classList.remove('vid');
 document.querySelector('.wrapper.travel').classList.remove('vid');
+// document.querySelector('.buttons-zakaz').classList.remove('vid');
 }
 function pricelist3(){
     elem = document.querySelector('.wrapper.travel');
+    blok = document.querySelector('.buttons-zakaz');
 elem.classList.toggle('vid');
+blok.classList.add('vid');
 document.querySelector('.wrapper.tech').classList.remove('vid');
 document.querySelector('.wrapper.avto').classList.remove('vid');
+// document.querySelector('.buttons-zakaz').classList.remove('vid');
 }
